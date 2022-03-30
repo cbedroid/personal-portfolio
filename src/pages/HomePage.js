a75beed7ccda9ec9b41f331f33cbd65f8dccbb34
cbedroid <cbedroid1614@gmail.com>
2022-03-30 03:56:23 -0400

    Added Resume Page, fixed imports order

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import About from "../components/About";
-import Hero from "../components/Hero";
-import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 
 const HomePage = () => {
   return (
-    <div className="-mt-1">
+    <div className="mt-8">
       <Hero />
       <About />
       <Projects />
