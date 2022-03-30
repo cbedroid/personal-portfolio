81ab8d829d13f816f40577251024cc4e846dbaa8
cbedroid <cbedroid1614@gmail.com>
2022-03-30 04:14:28 -0400

    Fixed footer link styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,18 +8,18 @@ const Footer = () => {
           <div className="header">
             <h2 className="text-4xl font-bold text-white border-b-4 border-blue-600 w-4/5 lg:w-1/5 pb-2">Stay Connected!</h2>
           </div>
-          <div className="contact-links flex flex-col lg:flex-row justify-between c space-y-6 text-gray-100 p-4 my-4 md:mt-16 ">
-            <a href="linkedin.com/in/cornelius-brooks" target="_blank" className="btn contact-details" rel="noreferrer">
+          <div className="contact-links flex flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 text-gray-100 p-4 my-4 md:mt-16 lg:items-center ">
+            <a className="btn floating-link" href="linkedin.com/in/cornelius-brooks" target="_blank" rel="noreferrer">
               <i className="fab fa-linkedin"></i> Linkedin
             </a>
-            <a id="profile-link" href="https://github.com/cbedroid" target="_blank" className="btn contact-details" rel="noreferrer">
+            <a className="btn floating-link" href="https://github.com/cbedroid" target="_blank" rel="noreferrer">
               <i className="fab fa-github"></i> GitHub
             </a>
-            <a href="https://twitter.com/cbedroid" target="_blank" className="btn contact-details" rel="noreferrer">
+            <a className="btn floating-link" href="https://twitter.com/cbedroid" target="_blank" rel="noreferrer">
               <i className="fab fa-twitter"></i> Twitter
             </a>
-            <a href="mailto:cbedroid1614@gmail.com" className="btn contact-details">
-              <i className="fas fa-at"></i> Send a mail
+            <a className="btn floating-link" href="mailto:cbedroid1614@gmail.com">
+              <i className="fas fa-at"></i> Email Me
             </a>
           </div>
         </div>
