378477b57742134c5a57e5149a9994d9f3f23ee0
cbedroid <cbedroid1614@gmail.com>
2022-03-30 04:11:58 -0400

    Updated ProjectCard layouts, Added TechAvatars for tech stack skills

--- src/components/subcomponents/ProjectCard.js
+++ src/components/subcomponents/ProjectCard.js
@@ -1,45 +1,33 @@
 import React from "react";
+import Chip from "@mui/material/Chip";
+import TechAvatars from "./TechAvatars";
 
 const ProjectCard = (props) => {
   return (
     <>
       {props?.src ? (
-        <div className="project-tile relative group bg-[#efedec] shadow-xl sm:max-w-lg sm:mx-auto rounded-xl p-2 ">
-          <div className="hidden group-hover:block absolute left-0 top-0 z-50 bg-white text-gray-800  h-full w-full sm:max-w-lg sm:mx-auto rounded-xl p-2 ">
-            <div className="flex flex-col items-center justify-center h-full">
-              <h2 className="text-2xl font-bold py-4">{props?.name}</h2>
-              <h4 className="text-xl pb-2 text-indigo-700 ">{props?.tech}</h4>
-              <a
-                disabled={!props?.link}
-                className={`font-medium leading-none rounded-l-none ${
-                  !props.link
-                    ? "bg-gray-300 text-gray-700"
-                    : "text-indigo-700  hover:bg-indigo-600 border-2 hover:text-white border-indigo-700"
-                }  text-center rounded  uppercase w-44 p-5`}
-                href={props?.link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {props?.link ? "View Work" : "Unavailable"}
-              </a>
-            </div>
-          </div>
-          <div className="max-w-md mx-auto">
-            <h4
-              tabIndex="0"
-              className="focus:outline-none text-gray-800 dark:text-gray-100 text-xl font-bold mb-3 p-2"
-            >
+        <div className="content-container flex flex-col items-between bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-100 border-2 border-gray-200 rounded-md">
+          <div className="content__header">
+            <video src={props?.src} autoPlay loop muted style={{ width: "700px" }} />
+            <h4 className="focus:outline-none bg-gray-200 text-gray-800 dark:text-gray-100 text-xl font-bold border-t border-b border-gray-300 mb-3 p-2 py-4">
               {props?.name}
             </h4>
-            <div className="divide-y divide-gray-400/50">
-              <video
-                src={props?.src}
-                autoPlay
-                loop
-                muted
-                style={{ width: "700px" }}
+          </div>
+          <div className="content__main flex flex-col items-start space-y-6 p-2">
+            <TechAvatars data={props?.tech || []} />
+            <p className="text-gray-800 dark:text-gray-100">{props?.description}</p>
+          </div>
+          <div className="content__footer flex justify-end items-baseline p-4 mt-auto  w-full">
+            <a disabled={!props?.link} href={props?.link} target="_blank" rel="noopener noreferrer">
+              <Chip
+                disabled={!props?.link}
+                className="font-semibold hover:ring-2 hover:ring-offset-2 hover:ring-blue-400 rounded-md shadow-sm hover:shadow-xl"
+                sx={{ fontSize: "18px", py: 2.5, px: 3 }}
+                color="primary"
+                size="large"
+                label={props?.link ? "View Work" : "Unavailable"}
               />
-            </div>
+            </a>
           </div>
         </div>
       ) : (
