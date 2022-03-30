c2ea49d20e6b553c658725e7c8631f68b4e1fd74
cbedroid <cbedroid1614@gmail.com>
2022-03-30 03:52:18 -0400

    Added resume router link to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,28 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React, { useState } from "react";
 
 const links = [
   {
     id: 1,
     name: "About",
-    href: "#about",
+    href: "/#about",
   },
   {
     id: 2,
     name: "Work",
-    href: "#projects",
+    href: "/#projects",
   },
   {
     id: 3,
     name: "Contact",
-    href: "#contact",
+    href: "/#contact",
+  },
+  {
+    id: 4,
+    name: "Resume",
+    href: "/resume",
   },
 ];
 
@@ -34,34 +42,23 @@ export default function Nav() {
 
   return (
     <div className="2xl:container 2xl:mx-auto">
-      <div
-        className="fixed top-0 left-0  bg-gray-800 text-white  border-b-2 border-gray-200 shadow h-24 w-full rounded py-5 px-7"
-        i
-        style={{ zIndex: 2000 }}
-      >
+      <div className="fixed top-0 left-0  bg-gray-800 text-white  border-b-2 border-gray-200 shadow h-24 w-full rounded py-5 px-7" i style={{ zIndex: 2000 }}>
         <div className="flex justify-content-between items-center">
           <nav className="flex justify-between items-center w-full">
-            <a
-              className="flex items-center space-x-3 lg:pr-16 pr-6 pt-4 md:pt-0"
-              href="#welcome-section"
-            >
+            <a className="flex items-center space-x-3 lg:pr-16 pr-6 pt-4 md:pt-0" href="#welcome-section">
               <img
                 className="rounded-full border border-gray-200 dark:border-gray-700 h-12 w-12"
-                src={require("../assets/avatar_head.png")}
+                src={require("../assets/images/avatar_head.png")}
                 alt="avatar"
               />
-              <h2 className="font-normal text-2xl leading-6 text-white dark:text-gray-800">
-                CbeDroid
-              </h2>
+              <h2 className="font-normal text-2xl leading-6 text-white dark:text-gray-800">CbeDroid</h2>
             </a>
             {/* For medium and plus sized devices */}
             <ul className="hidden md:flex self-end space-x-2">
               <li
                 onClick={() => selected(0)}
                 className={` md:hidden ${
-                  style[0]
-                    ? "text-white bg-indigo-600"
-                    : "text-gray-100 dark:text-gray-600 border border-white bg-gray-400"
+                  style[0] ? "text-white bg-indigo-600" : "text-gray-100 dark:text-gray-600 border border-white bg-gray-400"
                 }  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800  cursor-pointer px-3 py-2.5 font-normal text-xs leading-3 shadow-md rounded`}
               >
                 <svg
@@ -88,15 +85,10 @@ export default function Nav() {
                   key={index}
                   onClick={() => selected(index + 1)}
                   className={`group  ${
-                    style[index + 1]
-                      ? "text-white bg-indigo-600"
-                      : "text-gray-600 border border-white bg-gray-50"
+                    style[index + 1] ? "text-white bg-indigo-600" : "text-gray-600 border border-white bg-gray-50"
                   } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer  font-normal text-xs leading-3 shadow-md rounded`}
                 >
-                  <a
-                    className="block text-xl font-bold w-full h-full px-3 py-2.5"
-                    href={link?.href}
-                  >
+                  <a className="block text-xl font-bold w-full h-full px-3 py-2.5" href={link?.href}>
                     {link?.name}
                   </a>
                 </li>
@@ -106,14 +98,13 @@ export default function Nav() {
           {/* for smaller devices */}
           <div className="md:hidden self-end mt-5 ">
             <div
+              role="button"
+              tabIndex={0}
               onClick={() => setDropDown(!dropDown)}
               className="cursor-pointer text-center p-3 text-white bg-indigo-600 rounded flex justify-between items-center w-12 md:w-full"
             >
               <div className="flex space-x-2">
-                <p
-                  id="textClicked"
-                  className="font-normal text-sm leading-3 focus:outline-none hover:bg-gray-800 duration-100 cursor-pointer"
-                >
+                <p id="textClicked" className="font-normal text-sm leading-3 focus:outline-none hover:bg-gray-800 duration-100 cursor-pointer">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="icon icon-tabler icon-tabler-menu-2"
@@ -135,12 +126,7 @@ export default function Nav() {
               </div>
             </div>
             <div className="relative w-full">
-              <ul
-                id="list"
-                className={`${
-                  dropDown ? "hidden" : "block"
-                } absolute top-2 right-0 font-normal text-base leading-4  w-72 rounded shadow-md`}
-              >
+              <ul id="list" className={`${dropDown ? "hidden" : "block"} absolute top-2 right-0 font-normal text-base leading-4  w-72 rounded shadow-md`}>
                 {links.map((link, index) => (
                   <li
                     key={index}
