2df5b8156be29ae0467ad6719502909bcb591ed9
cbedroid <cbedroid1614@gmail.com>
2022-03-30 03:49:54 -0400

    Added React Router and material-ui theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
 import "./App.css";
 import "./static/css/styles.css";
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import HomePage from "./pages/HomePage";
+import ResumePage from "./pages/ResumePage";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#2196f3",
+      main: "#3f51b5",
+      dark: "#2c387e",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff9100",
+      main: "#E25115",
+      dark: "#ba000d",
+      contrastText: "#b22a00",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <HomePage />
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div className="App">
+          <Nav />
+          <main className="mt-24">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/resume" element={<ResumePage />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
