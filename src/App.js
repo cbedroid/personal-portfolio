c573d997689c8acb32cfece0d675616fa3cdcaee
cbedroid <cbedroid1614@gmail.com>
2022-03-31 02:01:54 -0400

    Fixed Router issues - Setup vercel deployment

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import "./App.css";
-import "./static/css/styles.css";
-import React from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-import HomePage from "./pages/HomePage";
-import ResumePage from "./pages/ResumePage";
+import './App.css'
+import './static/css/styles.css'
+import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import HomePage from './pages/HomePage'
+import ResumePage from './pages/ResumePage'
 
 const theme = createTheme({
   palette: {
     primary: {
-      light: "#2196f3",
-      main: "#3f51b5",
-      dark: "#2c387e",
-      contrastText: "#fff",
+      light: '#2196f3',
+      main: '#3f51b5',
+      dark: '#2c387e',
+      contrastText: '#fff',
     },
     secondary: {
-      light: "#ff9100",
-      main: "#E25115",
-      dark: "#ba000d",
-      contrastText: "#b22a00",
+      light: '#ff9100',
+      main: '#E25115',
+      dark: '#ba000d',
+      contrastText: '#b22a00',
     },
   },
-});
+})
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <div className="App">
-          <Nav />
-          <main className="mt-24">
+      <div className='App'>
+        <Nav />
+        <main className='mt-24'>
+          <Router>
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/resume" element={<ResumePage />} />
+              <Route path='/resume' element={<ResumePage />} />
+              <Route path='/' exact element={<HomePage />} />
             </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
+          </Router>
+        </main>
+      </div>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
