0ca635c751911d6a5ddafa8acd14e58391c07dc1
cbedroid <cbedroid1614@gmail.com>
2022-07-03 23:34:36 -0400

    eslint ignore jsx imports, added webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
+};
