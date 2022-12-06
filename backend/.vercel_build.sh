a6b226d035f92b889cd89dd8a68d5c7462b3a1ef
cbedroid <cbedroid1614@gmail.com>
2022-12-06 02:47:49 -0500

    Django and React build file for vercel deployment

---
+++ backend/.vercel_build.sh
@@ -0,0 +1,27 @@
+# Vercel Django Build Setup
+
+pip install -r requirements.dev.txt
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser --noinput
+python manage.py create_superuser_fixtures --noinput
+
+
+if [ "$run_fixtures" = "true" ];
+then
+  cat << EOF
+  |-------------------------------------|
+  |      Running Django Fixtures....    |
+  |-------------------------------------|
+EOF
+  python manage.py loaddata core
+  python manage.py loaddata social_account
+  python manage.py loaddata userprofile
+else
+  cat << EOF
+  |-------------------------------------|
+  |      Ignoring Django Fixtures...    |
+  |-------------------------------------|
+EOF
+fi;
---
+++ frontend/.vercel_build.sh
@@ -0,0 +1,6 @@
+# Vercel Node/React Build Setup
+
+npm cache clean -f
+npm install -g
+npm install @babel/eslint-parser
+npm build
