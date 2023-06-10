27d12bd049c08fd3cd1b58bac0720b2c3d4b5622
cbedroid <cbedroid1614@gmail.com>
2023-06-09 23:03:04 -0400

    update Resume responsiveness on smaller screens

--- frontend/src/containers/resume/ResumePage.tsx
+++ frontend/src/containers/resume/ResumePage.tsx
@@ -17,15 +17,12 @@ const ResumePage = () => {
   );
 
   return (
-    <div className="bg-gray-400 min-h-screen">
-      <div className="container mx-auto pt-8 px-2">
+    <div className="bg-gray-400 min-h-screen pt-6">
+      <div className="container mx-auto px-4">
         {resume && <ResumeTitle color="blue" resume={resume} />}
-        <div className="resume-wrapper mx-auto">
+        <div className="resume-wrapper flex justify-center">
           {resumeQuery.isSuccess && resume ? (
-            <Resume
-              className={"flex flex-col items-center content-center"}
-              file={resume.url}
-            />
+            <Resume file={resume.url} />
           ) : (
             <Box
               sx={{
